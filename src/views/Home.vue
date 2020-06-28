3f700210c11fdc99932f5b77394194f6f9eafef1
youtianle <tianleyou@foxmail.com>
2020-06-28 20:13:18 +0800

    相册的demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="home">
+    <div class="box">
+      <div class="item">上</div>
+      <div class="item">下</div>
+      <div class="item">左</div>
+      <div class="item">右</div>
+      <div class="item">前</div>
+      <div class="item">后</div>
+    </div>
   </div>
 </template>
 
@@ -15,7 +23,16 @@ export default {
 .home {
   width: 100vw;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background: url(~@/assets/img/home-bg.jpg) no-repeat center;
   background-size: auto 100%;
+}
+.box {
+  width: 500px;
+  height: 500px;
+  transform-style: preserve-3d;
+  background-color: aqua;
 }
 </style>
